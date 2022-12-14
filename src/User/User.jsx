08325d7e469b8d9391eeed8236740e22f75cd0b3
lwgover <lgover@pugetsound.edu>
2022-12-13 19:59:53 -0800

    reverted Georgia's userpage

--- src/User/User.jsx
+++ src/User/User.jsx
@@ -1,87 +1,36 @@
 import { Link } from "react-router-dom"
 import Parser from 'html-react-parser';
 
-export class QuizSearchPage extends React.Component {
-    constructor(){
-        super();
-        this.state={
-        };
-
-        this.UserPage = this.UserPage.bind(this);
-        this.UserStuff = this.UserStuff.bind(this);
-        this.displayUserResults = this.displayUserResults.bind(this);
-        this.queryDatabase = this.queryDatabase.bind(this);
+function UserPage({ username }) {
+    const style = {
+        margin: "auto",
+        padding: "0% 5% 10% 5%",
+        color: "Black"
     }
 
-    UserPage({ username }) {
-        const style = {
-            margin: "auto",
-            padding: "0% 5% 10% 5%",
-            color: "Black"
-        }
-
-        return <div style={style}>
-            <div style={{ "fontSize": "96px" }}>
-                {Parser(username)}
-            </div>
-            <UserStuff />
+    return <div style={style}>
+        <div style={{ "fontSize": "96px" }}>
+            {Parser(username)}
         </div>
+        <UserStuff />
+    </div>
+}
+
+function UserStuff({ username }) {
+
+    const style = {
+        margin: "auto",
+        padding: "10% 35% 10% 15%",
+        color: "black"
     }
 
-    UserStuff({ username }) {
-
-        const style = {
-            margin: "auto",
-            padding: "10% 35% 10% 15%",
-            color: "black"
-        }
-
-        return <div style={style}>
-            <div style={{ "fontSize": "48px" }}>
-                {this.displayUserResults(username)}
+    return <div style={style}>
+        <form onSubmit={localStorage.clear()}>
+            <div>
+                <button type="submit">log out</button>
             </div>
-            <br />
-            <form onSubmit={localStorage.clear()}>
-                <div>
-                    <button type="submit">log out</button>
-                </div>
-            </form>
-        </div>
-    }
-
-    async displayUserResults(username) {
-        if (username === "") {
-            return;
-        }
-
-        //let databaseSearch = database.searchQuizNames(this.state.value); //search the database
-        let databaseSearch = await this.queryDatabase(username);
-
-        console.log(databaseSearch);
-
-        if (databaseSearch === null) { //if the search didn't find anything,
-            this.setState({ searchResults: [] }); //set the results to an empty array
-        }
-
-        else {
-            this.setState({ searchResults: databaseSearch });
-        }
-    }
-
-    queryDatabase(username) {
-        console.log("query databse with " + username);
-
-        let searchTerm = { searchTerm: username };
-
-        return fetch('http://localhost:8080/SearchUsernames', {
-            method: 'POST',
-            headers: {
-                'Content-Type': 'application/json'
-            },
-            body: JSON.stringify(searchTerm)
-        })
-            .then(data => data.json())
-    }
+        </form>
+    </div>
 }
 
 export default UserPage
